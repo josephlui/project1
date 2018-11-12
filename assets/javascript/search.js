0afa90d228536b2eacdcd5dc2fc0a4220e6e4653
Joseph Lui <joseph.lui2000@gmail.com>
2018-11-11 21:41:56 -0500

    add item tracking

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -48,7 +48,7 @@ function parseResponse(json){
             `<img class="card-img-top" src="${imgURL}" alt="${name}" width="254" height="254">` +
             '<div class="card-body">'+
             `<h5 class="card-title">${name}</h5>` +
-            `<a href="#" class="btn btn-primary btnItem" id="${itemId}">Save Item</a>` +
+            `<a href="#" class="btn btn-primary btnItem" id="${itemId}"></a>` +
             '</div>'+
             '</div></div>';
     }
