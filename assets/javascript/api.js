a4dbc4dc7be42f77fe7f65811814d2d3c2356ab6
SaumilBapat <sbapat@salesforce.com>
2018-11-11 21:04:04 -0500

    Updated Empty Template

---
+++ assets/javascript/api.js
@@ -0,0 +1,24 @@
+//Save the user to Firebase
+function saveUser(user) {
+
+}
+
+//Retrieve the user from Firebase
+function retrieveUser(userId) {
+
+}
+
+//Subscribe the user to this Item in Firebase
+function subscribeItem(userId, itemId) {
+
+}
+
+//Unsubscribe the user from this item in Firebase
+function unsubscribeItem(userId, itemId) {
+
+}
+
+//Search the walmart api for this item and return a list of items
+function findItems(keyword) {
+  
+}
---
+++ assets/javascript/ui.js
@@ -0,0 +1,14 @@
+$( document ).ready(function() {
+  
+});
+
+
+//Render the given items in the UI
+function renderItems(keyword){
+  findItems();
+}
+
+//If the user email is registered, then redirect user to the search page
+function redirect() {
+
+}
